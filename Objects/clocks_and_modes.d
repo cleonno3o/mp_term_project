67a0d5c67cc40175dde417c1bcd67e27de80e55c
sumin <cleonn7878@gmail.com>
2024-12-04 11:01:34 +0900

    Initia lCommit

---
+++ Objects/clocks_and_modes.d
@@ -0,0 +1,9 @@
+./objects/clocks_and_modes.o: clocks_and_modes.c \
+  C:\Users\sumin\AppData\Local\Arm\Packs\Keil\S32_SDK_DFP\1.5.1\platform\devices\device_registers.h \
+  C:\Users\sumin\AppData\Local\Arm\Packs\Keil\S32_SDK_DFP\1.5.1\platform\devices\common\s32_core_cm4.h \
+  C:\Users\sumin\AppData\Local\Arm\Packs\Keil\S32_SDK_DFP\1.5.1\platform\devices\S32K144\include\S32K144.h \
+  C:\Users\sumin\AppData\Local\Arm\Packs\ARM\CMSIS\5.5.0\CMSIS\Core\Include\core_cm4.h \
+  RTE\Device\S32K144HAxxxLHx\system_S32K144.h \
+  C:\Users\sumin\AppData\Local\Arm\Packs\Keil\S32_SDK_DFP\1.5.1\platform\devices\S32K144\include\S32K144_features.h \
+  C:\Users\sumin\AppData\Local\Arm\Packs\Keil\S32_SDK_DFP\1.5.1\platform\devices\devassert.h \
+  clocks_and_modes.h
