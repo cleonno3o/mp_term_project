bd6c3ad8b1e4aece5527dc14ac2d71cde6a70c3d
sumin <cleonn7878@gmail.com>
2024-12-08 02:49:04 +0900

    Feat: 부저 헤더 구현

---
+++ buzzer.h
@@ -0,0 +1,11 @@
+#include "S32K144.h"
+
+#define BUZZER 1
+#define BUZZER_PTN PTE
+void buzzer_set(bool on)
+{
+    if (on)
+        BUZZER_PTN->PSOR = 1 << BUZZER;
+    else
+        BUZZER_PTN->PCOR = 1 << BUZZER;
+}
