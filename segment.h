228656c6c56a1b5fd692b4236a6e669387337f78
sumin <cleonn7878@gmail.com>
2024-12-08 19:39:03 +0900

    Refactor: 세그먼트 리팩터링

--- segment.h
+++ segment.h
@@ -7,34 +7,26 @@ typedef struct {
     unsigned int fnd_data[10];
     unsigned int fnd_sel[4];
 } Segment;
+#define SEGMENT_PTN PTC
 
-void segment_init(Segment *segment, PORT_Type *port_type, GPIO_Type *ptn, int pcc_index) {
-    segment->PORTN = port_type;
-    segment->PTN = ptn;
-    segment->PCC_INDEX = pcc_index;
-    PCC->PCCn[segment->PCC_INDEX] = PCC_PCCn_CGC_MASK;
-    
-    // segment 포트 설정
-    for (int i = 1; i <= 7; i++) {
-        segment->PORTN->PCR[i] = PORT_PCR_MUX(1);
-        segment->PTN->PDDR |= 1 << i;
-    }
-    // com 포트 설정
-    for (int i = 8; i <= 11; i++) {
-        segment->PORTN->PCR[i] = PORT_PCR_MUX(1);
-        segment->PTN->PDDR |= 1 << i;
-    }
-}
-
-void set_num(Segment *segment, int num, int pos)
+void set_num (int num, int pos)
 {
     unsigned int fnd_data[10]={0x7E, 0x0C,0xB6,0x9E,0xCC,0xDA,0xFA,0x4E,0xFE,0xDE};// 0~9 number
     unsigned int fnd_sel[4]={0x0100,0x0200,0x0400,0x0800};
-    
-    segment->PTN->PCOR |= 0xFE;          // clear data output
-    segment->PTN->PSOR |= fnd_sel[pos];  // ouput com port select
-    segment->PTN->PCOR |= 0xFE;          // clear data output
-    segment->PTN->PSOR |= fnd_data[num]; // set data output
+
+    SEGMENT_PTN->PCOR |= 0xFE;          // clear data output
+    SEGMENT_PTN->PSOR |= fnd_sel[pos];  // ouput com port select
+    SEGMENT_PTN->PCOR |= 0xFE;          // clear data output
+    SEGMENT_PTN->PSOR |= fnd_data[num]; // set data output
     delay_ms(2);
-    segment->PTN->PCOR |= 0xFFF; // clear all
+    SEGMENT_PTN->PCOR |= 0xFFF; // clear all
+}
+
+void print_4_digit(int num)
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        set_num(num % 10, 4 - i);
+        num /= 10;
+    }
 }
